f7b134b5597abbae9d56151c1c2a4b4ba867a43c
Jacob J <96319709+night780@users.noreply.github.com>
2023-03-16 11:34:24 -0700

    Update EyeBreak.bat

--- EyeBreak.bat
+++ EyeBreak.bat
@@ -2,8 +2,8 @@
 echo Eye Break program is running...
 echo Press Ctrl+C to exit the program.
 :loop
-rem Set the time delay to 1 hour (3600 seconds)
-timeout /t 1800 /nobreak >nul
+rem Set the time delay to 20 min (3600 seconds = 1 hour, 1200 = 20min)
+timeout /t 1200 /nobreak >nul
 rem Send a notification to remind you to take an eye break
 echo Set objShell = CreateObject("WScript.Shell") > "%temp%\popup.vbs"
 echo Set objFSO = CreateObject("Scripting.FileSystemObject") >> "%temp%\popup.vbs"
